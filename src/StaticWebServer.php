62d134580a6d61c216645fc6e775ba100aae7a1a
Sam Stenvall <sam.stenvall@nordsoftware.com>
2016-10-28 20:52:43 +0300

    Log failed requests as errors

--- src/StaticWebServer.php
+++ src/StaticWebServer.php
@@ -193,10 +193,18 @@ class StaticWebServer
 
                 $response->end(file_get_contents($filePath));
             } else {
+                $this->logger->error('HTTP request failed, file unreadable ({filePath})', [
+                    'filePath' => $filePath,
+                ]);
+                
                 $response->writeHead(403, ['Content-Type' => 'text/plain']);
                 $response->end("Forbidden\n");
             }
         } else {
+            $this->logger->error('HTTP request failed, file not found ({filePath})', [
+                'filePath' => $filePath,
+            ]);
+            
             $response->writeHead(404, ['Content-Type' => 'text/plain']);
             $response->end("Not found\n");
         }
